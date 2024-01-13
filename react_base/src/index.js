a3d25e155caaf401bf2dbce7e90fee8980f3b45d
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-13 17:26:23 -0300

    Class 209 - React Router DOM

--- react_base/src/index.js
+++ react_base/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
