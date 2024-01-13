79054bdb462ff10604467ebd68d2fcb3959f1501
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-13 14:53:56 -0300

    Class 205 - Styled-Components

---
+++ react_base/src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from '../../styles/GlobalStyles';
+import { Title } from './style';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>Login</Title>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+    </Container>
+  );
+}
