c4f348b828ee58a8847d6cb184eb839ad7ae321e
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-13 20:51:01 -0300

    Class 215 - Redux + React

--- react_base/src/pages/Login/index.js
+++ react_base/src/pages/Login/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Container } from '../../styles/GlobalStyles';
 import { Title } from './style';
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleClick(event) {
+    event.preventDefault();
+    dispatch({
+      type: 'BOTAO_CLICADO',
+    });
+  }
+
   return (
     <Container>
       <Title>Login</Title>
+
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-      <button type="button">Enviar</button>
+
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
     </Container>
   );
 }
