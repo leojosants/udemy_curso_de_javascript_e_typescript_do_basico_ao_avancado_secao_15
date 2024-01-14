4e61bf9f3cfe9bb9e095911f4eed9eaf19ca0dc3
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-14 19:07:52 -0300

    Class 217 - Redux Saga

--- react_base/src/pages/Login/index.js
+++ react_base/src/pages/Login/index.js
@@ -2,22 +2,20 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Container } from '../../styles/GlobalStyles';
 import { Title } from './style';
-import * as exampleAction from '../../store/modules/example/actions';
+import * as exampleActions from '../../store/modules/example/actions';
 
 export default function Login() {
   const dispatch = useDispatch();
 
   function handleClick(event) {
     event.preventDefault();
-    dispatch(exampleAction.clicaBotao());
+    dispatch(exampleActions.clicaBotaoRequest());
   }
 
   return (
     <Container>
       <Title>Login</Title>
-
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-
       <button type="button" onClick={handleClick}>
         Enviar
       </button>
