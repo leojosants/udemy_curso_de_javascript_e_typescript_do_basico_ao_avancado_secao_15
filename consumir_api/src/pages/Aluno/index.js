b50d67fd789fa14d8a0103c9c44b494f4c739fe2
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-16 11:30:29 -0300

    Class 232 - Student registration: part 2

--- consumir_api/src/pages/Aluno/index.js
+++ consumir_api/src/pages/Aluno/index.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { get } from 'lodash';
+import { isEmail, isInt, isFloat } from 'validator';
 import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
 import { Container } from '../../styles/GlobalStyles';
 import { Form } from './style';
+import Loading from '../../components/Loading';
+import axios from '../../services/axios';
+import history from '../../services/history';
 
 export default function Aluno({ match }) {
   const id = get(match, 'params.id', 0);
@@ -12,13 +17,72 @@ export default function Aluno({ match }) {
   const [idade, setIdade] = useState('');
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!id) return;
+    async function getData() {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`/alunos/${id}`);
+        const Foto = get(data, 'Fotos[0].url', '');
+        setNome(data.nome);
+        setSobrenome(data.sobrenome);
+        setEmail(data.email);
+        setIdade(data.idade);
+        setPeso(data.peso);
+        setAltura(data.altura);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        const status = get(err, 'response.status', 0);
+        const errors = get(err, 'response.data.errors', []);
+        if (status === 400) errors.map((error) => toast.error(error));
+        history.push('/');
+      }
+    }
+    getData();
+  }, [id]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    let formErrors = false;
+
+    if (nome.length < 3 || nome.length > 255) {
+      formErrors = true;
+      toast.error('NOME deve ter entre 3 e 255 caracteres!');
+    }
+
+    if (sobrenome.length < 3 || sobrenome.length > 255) {
+      formErrors = true;
+      toast.error('SOBRENOME deve ter entre 3 e 255 caracteres!');
+    }
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('E-MAIL inválido!');
+    }
+
+    if (!isInt(String(idade))) {
+      formErrors = true;
+      toast.error('IDADE inválida!');
+    }
+
+    if (!isFloat(String(peso))) {
+      formErrors = true;
+      toast.error('PESO inválido!');
+    }
+
+    if (!isFloat(String(altura))) {
+      formErrors = true;
+      toast.error('ALTURA inválida!');
+    }
   };
 
   return (
     <Container>
+      <Loading isLoading={isLoading} />
+
       <h1>{id ? 'Editar aluno' : 'Novo aluno'}</h1>
 
       <Form onSubmit={handleSubmit}>
@@ -55,6 +119,7 @@ export default function Aluno({ match }) {
             id="email"
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Digite o e-mail"
+            autoComplete="true"
           />
         </label>
 
